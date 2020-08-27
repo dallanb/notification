56debaeb03fd8d8b02fa94c787efd5e05e7ba680
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-08-27 10:16:16 -0700

    create first attempt at producer by following cloudamqp's guide

--- src/libs/rabbitmq/producer.ts
+++ src/libs/rabbitmq/producer.ts
@@ -11,6 +11,9 @@ class Producer {
         username: string;
         vhost: string;
     };
+    private _channel: any;
+    private _offlinePubQueue: any[];
+
     constructor() {
         this._connection = undefined;
         this._connection_options = {
@@ -21,6 +24,8 @@ class Producer {
             password: config.RABBITMQ_PASSWORD,
             vhost: config.RABBITMQ_VHOST,
         };
+        this._channel = undefined;
+        this._offlinePubQueue = [];
     }
 
     get connection(): amqp.Connection {
@@ -29,6 +34,22 @@ class Producer {
 
     set connection(conn: amqp.Connection) {
         this._connection = conn;
+    }
+
+    get channel(): amqp.ConfirmChannel {
+        return this._channel;
+    }
+
+    set channel(channel: amqp.ConfirmChannel) {
+        this._channel = channel;
+    }
+
+    get offlinePubQueue(): any[] {
+        return this._offlinePubQueue;
+    }
+
+    set offlinePubQueue(offlinePubQueue) {
+        this._offlinePubQueue = offlinePubQueue;
     }
 
     connect(): void {
@@ -41,6 +62,69 @@ class Producer {
                 this.connection = connection;
             }
         );
+    }
+
+    startPublisher(): void {
+        this.connection.createConfirmChannel(
+            (err: Error, ch: amqp.ConfirmChannel) => {
+                if (this.closeOnErr(err)) {
+                    return;
+                }
+
+                ch.on('error', (err: Error): void =>
+                    console.error('[AMQP] channel closed')
+                );
+
+                ch.on('close', (): void =>
+                    console.log('[AMQP] channel closed')
+                );
+
+                this.channel = ch;
+
+                while (true) {
+                    const m = this.offlinePubQueue.shift();
+                    if (!m) break;
+                    this.publish(m[0], m[1], m[2]);
+                }
+            }
+        );
+    }
+
+    // method to publish a message, will queue messages internally if the connection is down and resend later
+    publish(exchange: string, routingKey: string, content: any): void {
+        try {
+            this.channel.publish(
+                exchange,
+                routingKey,
+                content,
+                {
+                    persistent: true,
+                },
+                (err: Error, ok: any): void => {
+                    if (err) {
+                        console.error('[AMQP] publish', err);
+                        this.offlinePubQueue.push([
+                            exchange,
+                            routingKey,
+                            content,
+                        ]);
+                        this.channel.close((err: Error) =>
+                            console.error('[AMQP] close', err)
+                        );
+                    }
+                }
+            );
+        } catch (e) {
+            console.error('[AMQP] publish', e.message);
+            this.offlinePubQueue.push([exchange, routingKey, content]);
+        }
+    }
+
+    closeOnErr(err: Error): boolean {
+        if (!err) return false;
+        console.error('[AMQP] error', err);
+        this.connection.close();
+        return true;
     }
 }
 
