38e0545b62c8dcb49f6edb55ed0a25508ee435b7
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-09-19 20:26:27 -0700

    some temp code to test participant invite

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -5,14 +5,14 @@ class Auth {
     handleEvent(key: Message['key'], value: Message['value']): void {
         console.log(key);
         console.log(value);
-        const { username, device_id } =
+        const { uuid, device_id } =
             typeof value === 'string' ? JSON.parse(value) : value.toString();
         switch (key) {
             case 'user_login':
-                RedisClient.set(username, JSON.stringify({ device_id }));
+                RedisClient.set(uuid, JSON.stringify({ device_id }));
                 break;
             case 'user_ping':
-                RedisClient.get(username).then((reply: string) =>
+                RedisClient.get(uuid).then((reply: string) =>
                     RabbitMQProducer.publish('web', 'direct', reply)
                 );
                 break;
--- src/services/contest.ts
+++ src/services/contest.ts
@@ -1,9 +1,21 @@
 import { Message } from 'kafka-node';
+import { RabbitMQProducer, RedisClient } from '../libs';
+import { logger } from '../common';
+
 
 class Contest {
     handleEvent = (key: Message['key'], value: Message['value']) => {
         console.log(key);
         console.log(value);
+        switch(key){
+            case 'participant_invited':
+                const {user_uuid} = typeof value === 'string' ? JSON.parse(value) : value.toString();
+                RedisClient.get(user_uuid).then((reply: string) => {
+                    logger.info(reply);
+                    RabbitMQProducer.publish('web', 'direct', reply);
+                });
+                break
+        }
     };
 }
 
