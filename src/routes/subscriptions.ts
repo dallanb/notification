8c83730362755404cc28cea947c19ca63e183b06
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-10-09 22:56:15 -0700

    add an api call for check if subscribed to a topic

--- src/routes/subscriptions.ts
+++ src/routes/subscriptions.ts
@@ -4,6 +4,7 @@ import { Subscriptions } from '../controllers';
 
 const router = express.Router();
 
+router.get('/', Subscriptions.fetch);
 router.post('/subscribe', Subscriptions.subscribe);
 router.delete('/unsubscribe', Subscriptions.unsubscribe);
 
